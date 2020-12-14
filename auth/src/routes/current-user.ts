0adc45dc775f950bc9233b6447fc61bd3aa6dd7b
ChihHaoChen <mygo@MyGode-MacBook-Pro.local>
2020-12-14 18:02:42 +0800

    Update the currentUserRouter and the signoutRouter, as well as some middelware codes.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,12 @@
 import express, { response } from 'express';
 
+import { currentUser } from '../middleware/current-user';
+
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-	res.send('Hi, THERE!');
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+	
+	res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express, { response } from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-	res.send('Hi, THERE!');
+	req.session = null;
+
+	res.send({});
 });
 
 export { router as signoutRouter };
